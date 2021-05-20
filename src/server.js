0169af56217d765a8ad21451dd04d82816d9fb23
Manuel Arnold <arnold@sipgate.de>
2021-05-20 11:14:54 +0200

    feat: compare call data with json data
    Co-authored-by: Florian Mai <mai@sipgate.de>
    Co-authored-by: Otto Bo-Shao Lin <lin@sipgate.de>

--- src/server.js
+++ src/server.js
@@ -1,37 +1,46 @@
-require('dotenv').config();
-const { createWebhookModule } = require('sipgateio');
+require("dotenv").config();
+const { createWebhookModule } = require("sipgateio");
 const fs = require("fs");
-const util = require("util")
+const util = require("util");
 
 const readFile = util.promisify(fs.readFile);
+const contacts = async () => {
+  const contactsData = await readFile("contacts.json");
+  return JSON.parse(contactsData);
+};
 
 const port = 8080;
+
 const serverAddress =
-	process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS || 'https://example.com:8080';
+  process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS || "https://example.com:8080";
 
 const webhookModule = createWebhookModule();
 webhookModule
-	.createServer({
-		port,
-		serverAddress,
-	})
-	.then(webhookServer => {
-		console.log(
-			`Server running at ${serverAddress}\n` +
-				'Please set this URL for incoming calls at https://console.sipgate.com/webhooks/urls\n' +
-				"ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
-				'Ready for calls 📞'
-		);
+  .createServer({
+    port,
+    serverAddress,
+  })
+  .then((webhookServer) => {
+    console.log(
+      `Server running at ${serverAddress}\n` +
+        "Please set this URL for incoming calls at https://console.sipgate.com/webhooks/urls\n" +
+        "ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
+        "Ready for calls 📞"
+    );
 
-		webhookServer.onNewCall(async (newCallEvent) => {
-			try {
-				const contactsData = await readFile("contacts.json");
-				const contacts = JSON.parse(contactsData);
-				console.log(contacts)
-			} catch (error) {
-				console.error("Could not find contacts.json: " + error.message);
-			  }
+    webhookServer.onNewCall(async (newCallEvent) => {
+      try {
+        let name = "Unknown";
 
-			console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
-		});
-	});
+        if (contacts[newCallEvent.from]) {
+          name = contacts[newCallEvent.from].name;
+        }
+
+        console.log(`Call from ${name}`);
+      } catch (error) {
+        console.error("Could not find contacts.json: " + error.message);
+      }
+
+      console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
+    });
+  });
