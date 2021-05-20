0566673224aeabe0a95507eb7f33e5aa40a66670
Manuel Arnold <arnold@sipgate.de>
2021-05-20 11:01:38 +0200

    feat: read json for call data
    Co-authored-by: Florian Mai <mai@sipgate.de>
    Co-authored-by: Otto Bo-Shao Lin <lin@sipgate.de>

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const { createWebhookModule } = require('sipgateio');
+const fs = require("fs");
+const util = require("util")
+
+const readFile = util.promisify(fs.readFile);
 
 const port = 8080;
 const serverAddress =
@@ -18,7 +22,16 @@ webhookModule
 				"ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
 				'Ready for calls 📞'
 		);
-		webhookServer.onNewCall(newCallEvent => {
+
+		webhookServer.onNewCall(async (newCallEvent) => {
+			try {
+				const contactsData = await readFile("contacts.json");
+				const contacts = JSON.parse(contactsData);
+				console.log(contacts)
+			} catch (error) {
+				console.error("Could not find contacts.json: " + error.message);
+			  }
+
 			console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
 		});
 	});
