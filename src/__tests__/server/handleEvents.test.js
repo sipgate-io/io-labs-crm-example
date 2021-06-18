38c4eeca0e64d261f47966330e51e4ce3380d8e3
mschwarz <mschwarz@sipgate.de>
2021-06-18 11:10:51 +0200

    test: add test for newCallEventHandler to test if number is correct or not
    Co-authored-by: Florian Mai <mai@sipgate.de>
    Co-authored-by: Manuel Arnold <arnold@sipgate.de>

--- src/__tests__/server/handleEvents.test.js
+++ src/__tests__/server/handleEvents.test.js
@@ -1,5 +1,7 @@
 import {createHandleNewCallEvent} from "../../server/event/newCallEventHandler";
 import {createHandleHangUpEvent} from "../../server/event/hangUpEventHandler";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 test("handleNewCallEvent sends a message with label 'incoming' once", async () => {
     const testNewCallEvent = {
@@ -21,6 +23,80 @@ test("handleNewCallEvent sends a message with label 'incoming' once", async () =
     expect(sendMessageMock).toHaveBeenCalledTimes(1);
 });
 
+test("handleNewCallEvent sets parameter correctly if number is found in contacts", async () => {
+    const testNewCallEvent = {
+        callId: '',
+        direction: 'in',
+        event: 'newCall',
+        from: '+491234567890',
+        'fullUserId[]': ['123456789'],
+        originalCallId: '',
+        to: '49999999',
+        'user[]': ['TestUser'],
+        'userId[]': ['123456789'],
+        xcid: '',
+    }
+
+    let number, name, surname, company;
+    const sendMessageMock = jest.fn((label, data) => {
+        number = data.number;
+        name = data.name;
+        surname = data.surname;
+        company = data.company;
+    });
+
+    const handleNewCallEvent = createHandleNewCallEvent(sendMessageMock, {
+        "+491234567890": {
+            "name": "Erika",
+            "surname": "Mustermann",
+            "company": "Mustermann GmbH"
+        }
+    })
+
+    await handleNewCallEvent(testNewCallEvent);
+    expect(number).toEqual("+491234567890");
+    expect(name).toEqual("Erika");
+    expect(surname).toEqual("Mustermann");
+    expect(company).toEqual("Mustermann GmbH");
+});
+
+test("handleNewCallEvent sets parameter to unknown if number is not found in contacts", async () => {
+    const testNewCallEvent = {
+        callId: '',
+        direction: 'in',
+        event: 'newCall',
+        from: '+491234567890',
+        'fullUserId[]': ['123456789'],
+        originalCallId: '',
+        to: '49999999',
+        'user[]': ['TestUser'],
+        'userId[]': ['123456789'],
+        xcid: '',
+    }
+
+    let number, name, surname, company;
+    const sendMessageMock = jest.fn((label, data) => {
+        number = data.number;
+        name = data.name;
+        surname = data.surname;
+        company = data.company;
+    });
+
+    const handleNewCallEvent = createHandleNewCallEvent(sendMessageMock, {
+        "+499999999999": {
+            "name": "Erika",
+            "surname": "Mustermann",
+            "company": "Mustermann GmbH"
+        }
+    })
+
+    await handleNewCallEvent(testNewCallEvent);
+    expect(number).toEqual("+491234567890");
+    expect(name).toEqual("unknown");
+    expect(surname).toEqual("unknown");
+    expect(company).toEqual("unknown");
+});
+
 test("handleHangUpEvent sends a message with label 'hangup' once", async () => {
 
     const testHangUpEvent = {
@@ -30,7 +106,7 @@ test("handleHangUpEvent sends a message with label 'hangup' once", async () => {
         from: "12345567",
         to: "4915791234567",
         direction: "in",
-        answeringNumber: "12345678",
+        answeringNumber: "12345678"
     }
 
     const historyClient = {
@@ -39,6 +115,7 @@ test("handleHangUpEvent sends a message with label 'hangup' once", async () => {
                 source: "12345567",
                 target: "12345678",
                 status: "PICKUP",
+                recordingUrl: "https://static.sipgate.com/examples/wav/example.wav"
             }
         }
     }
@@ -52,6 +129,8 @@ test("handleHangUpEvent sends a message with label 'hangup' once", async () => {
         }
     }, historyClient)
 
-    await handleHangUpEvent({});
+    await handleHangUpEvent(testHangUpEvent);
     expect(sendMessageMock).toHaveBeenCalledTimes(1);
 });
+
+
